3b6a4985545e208eac83a4633cfabc80f8abceb5
jack <jbroncato@luc.edu>
2020-02-08 19:53:10 -0600

    event object created

---
+++ Event.py
@@ -0,0 +1,6 @@
+class event:
+
+    def __init__(self,eventName,eventTime,eventDate):
+        self.name = eventName
+        self.time = eventTime
+        self.date = eventDate
--- csvReader.py
+++ csvReader.py
@@ -1,6 +1,9 @@
 import csv
+from Event import event
 #reads csv and prints it
 with open('Homework.csv', newline = '') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
-        print (', '.join(row))
+        newEvent = event(row[0],row[1],row[2])
+        print (newEvent.name)
+        print (newEvent.date)
