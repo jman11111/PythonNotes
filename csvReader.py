a454f2157b7c1aee2ad312b649ccd1e94a2150c8
jack <jbroncato@luc.edu>
2020-02-17 05:12:46 -0600

    Now events are stored in an array

--- csvReader.py
+++ csvReader.py
@@ -1,10 +1,14 @@
 import csv
 from Event import event
 #reads csv and prints it
+Events = []
 with open('Homework.csv', newline = '') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         newEvent = event(row[0],row[1],row[2])
-        print (newEvent.name)
-        print (newEvent.date)
-        print (newEvent.time)
+        Events.append(newEvent)
+
+for currEvent in Events:      
+    print (currEvent.name)
+    print (currEvent.date)
+    print (currEvent.time)
