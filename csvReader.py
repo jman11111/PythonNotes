48b832529d3ebbcd1262e1a1b02070e3b034b8f0
jman11111 <jbroncato@yahoo.com>
2020-03-11 14:28:46 -0500

    starting calendar storage system

--- csvReader.py
+++ csvReader.py
@@ -1,6 +1,23 @@
 import csv
 from Event import event
 #reads csv and prints it
+#construct months(28-31 days)
+January = [[] * 5 for i in range(31)]
+February = []
+March = []
+April = []
+May = []
+June = []
+July = []
+August = []
+September = []
+October = []
+November = []
+December = []
+
+#construct year(12 months)
+Calendar = [January,February,March,April,May,June,July,August,September,October,November,December]
+
 Events = []
 with open('Homework.csv', newline = '') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -8,7 +25,12 @@ with open('Homework.csv', newline = '') as csvfile:
         newEvent = event(row[0],row[1],row[2])
         Events.append(newEvent)
 
-for currEvent in Events:      
-    print (currEvent.name)
-    print (currEvent.date)
-    print (currEvent.time)
+for currEvent in Events:
+    date = currEvent.date.split("/")
+    dateMonth = date[0]
+    dateDay = date[1]
+    print(dateMonth)
+    print(dateDay)
+    #print (currEvent.name)
+    #print (currEvent.date)
+    #print (currEvent.time)
